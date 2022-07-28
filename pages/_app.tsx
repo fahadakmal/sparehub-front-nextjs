2ee2fa06336c1acebb610b9248e38360fa40dd76
Hammad <hammad@risingtech.com>
2022-07-28 16:56:23 +0500

    redux folder move outside pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css';
-import "@fontsource/mulish";
-import store from './redux/store';
+import '@fontsource/mulish';
+import store from '../redux/store';
 import { Provider } from 'react-redux';
 import './config/i18next';
 
 function App({ Component, pageProps }: AppProps) {
-  
-  return (<Provider store={store} >
-     <Component {...pageProps} />
-     </Provider>)
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default App
+export default App;
