742d6bd9486146f2d25577cef4f2bcf9be63b5de
awais <awaisshahid2014@outlook.com>
2022-08-10 15:53:08 +0500

    SHUB-98 prettier and pass strength

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.css';
-import "@fontsource/mulish";
+import '@fontsource/mulish';
 import store from '../src/redux/store';
 import { Provider } from 'react-redux';
 import '../src/config/i18next';
@@ -9,16 +9,14 @@ import React from 'react';
 import Head from 'next/head';
 
 function App({ Component, pageProps }: AppProps) {
-  
-  return (<Provider store={store} >
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-        </Head>
-        <Component {...pageProps} />
-       </Provider>)
+  return (
+    <Provider store={store}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      </Head>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default App;
