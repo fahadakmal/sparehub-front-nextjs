4bb4998654641d934ce5f49146d21f41fc8bf083
Sohaib Asghar <sohaib@devigital.com>
2022-08-15 13:13:42 +0500

    resend code for email completed

--- src/views/Signup/Congratulations.tsx
+++ src/views/Signup/Congratulations.tsx
@@ -14,7 +14,6 @@ const useStyles = {
     justifyContent: 'center',
     textAlign: 'center',
     gap: 3,
-    maxWidth: 455,
   },
 };
 
@@ -30,7 +29,7 @@ const Congratulations = ({ translate }: any) => {
         <Typography fontSize={24} fontWeight={700} lineHeight={'31px'} color="#2E303D">
           {translate('REGISTERED_SUCCESSFULLY')}
         </Typography>
-        <Typography color={'#292D3260'} fontSize={16} align="center" letterSpacing={0.32}>
+        <Typography sx={{ maxWidth: '419px' }} color={'#292D32'} fontSize={16} align="center" letterSpacing={0.32}>
           {translate('REGISTERED_SUCCESSFULLY_MESSAGE')}
         </Typography>
         <PrimaryButton onClick={handleNavigate} fullWidth>
