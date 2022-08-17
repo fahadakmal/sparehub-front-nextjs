813d1e92ee2c0e84d6130ddcff21908211870a6e
awais <awaisshahid2014@outlook.com>
2022-08-17 10:19:02 +0500

    forgot (phone and email) on same screen

--- src/views/Signup/OTPVerification.tsx
+++ src/views/Signup/OTPVerification.tsx
@@ -7,9 +7,12 @@ import ToastAlert from '../../components/Toast/ToastAlert';
 
 const OTPVerification = (props: any) => {
   const router = useRouter();
-  const { phoneNumber, email }: any = router.query;
+  const { phoneNumber, email, newPassword }: any = router.query;
+  // const data = router.query;
+  console.log(newPassword, 'abcd');
   const verificationType = phoneNumber ? phoneNumber : email;
   const identity = phoneNumber ? 'PHONE_NUMBER' : 'EMAIL';
+  console.log(verificationType, 'verificationtype');
   const auth: any = useAuth();
   const [otp, setOtp] = useState('');
   const [toast, setToast] = useState({
@@ -25,6 +28,7 @@ const OTPVerification = (props: any) => {
   const handleSubmit = async () => {
     try {
       const res = await auth.otpConfirmation(verificationType, otp);
+      // const res1 = await auth.confirmPassword(email, otp, newPassword);
       if (res) {
         router.push('/congratulations');
       }
@@ -63,6 +67,20 @@ const OTPVerification = (props: any) => {
       }
     } 
   };
+  const confirmPassword = async () => {
+    try {
+      console.log('confirm pass start');
+      const res = await auth.confirmPassword(verificationType, otp, newPassword);
+      console.log(res, 'confirm pass after');
+      if (res) {
+        router.push('/congratulations');
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        window.alert(err.message);
+      }
+    }
+  };
 
   const handleCloseToast = () => {
     setToast({ ...toast, appearence: false });
@@ -74,9 +92,11 @@ const OTPVerification = (props: any) => {
         handleChange={handleChange}
         handleSubmit={handleSubmit}
         phoneNumber={verificationType}
+        // email={verificationType}
         {...props}
         identity={identity}
         resendOtp={resendOtp}
+        confirmPassword={confirmPassword}
       />
       <ToastAlert
         appearence={toast.appearence}
