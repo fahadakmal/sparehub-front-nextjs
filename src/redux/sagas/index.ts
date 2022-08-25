e02837936182b6179345a3d2d54e9809d5ca0e08
Shakeel Ur Rehman <shakeel@risingtech.co>
2022-08-25 18:29:18 +0500

    integrated 3 apis related to seller onboarding

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,5 +1,6 @@
 import { all } from 'redux-saga/effects';
 import authSaga from './authSaga';
+import addressSaga from './addressSaga';
 export default function* rootSaga() {
-  yield all([authSaga()]);
+  yield all([authSaga(),addressSaga()]);
 }
