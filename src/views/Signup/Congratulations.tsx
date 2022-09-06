b6ca73c25e1d449ce46248b87640d8ef179fdd03
awais <awaisshahid2014@outlook.com>
2022-09-06 15:04:23 +0500

    function written for automatic redirection to take login page

--- src/views/Signup/Congratulations.tsx
+++ src/views/Signup/Congratulations.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Grid, Typography } from '@mui/material';
 import AuthContainer from '../../components/AuthContainer/AuthContainer';
 import { PrimaryButton } from '../../components/Button/PrimaryButton';
-import { useRouter } from 'next/router';
+import { Router, useRouter } from 'next/router';
 import ToastAlert from '../../components/Toast/ToastAlert';
 import LANG_STRINGS from '../../enums/langStrings';
 
@@ -31,6 +31,9 @@ const Congratulations = ({ translate }: any) => {
 
   useEffect(() => {
     setToast({ ...toast, message: translate(LANG_STRINGS.USER_VERIFIED), appearence: true, type: 'success' });
+    setTimeout(() => {
+      router.push('/login');
+    }, 3000);
   }, []);
 
   const handleNavigate = () => {
